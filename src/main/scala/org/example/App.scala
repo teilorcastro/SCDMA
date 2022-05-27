485e4b5c16d01aec8e1e1b7511a095400654c421
Teilor Castro <teilor.castro@pue.es>
2022-05-27 13:43:53 +0200

    commit initial

---
+++ src/main/scala/org/example/App.scala
@@ -0,0 +1,19 @@
+package org.example
+import org.apache.spark.SparkConf
+
+import org.apache.spark.sql.{SparkSession, functions}
+
+object App {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SCDMA: Compact Parquet Files")
+    val spark = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
+    val sc = spark.sparkContext
+
+    // Lectura de tabla
+    val df = spark.sql("SELECT * FROM ...")
+
+
+
+    println("df format ahora")
+  }
+}
